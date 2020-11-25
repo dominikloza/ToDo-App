041220916ee75060905ee5e29ead90b2bb65349a
Dominik <dominikloza110899@gmail.com>
2020-11-25 20:00:38 +0100

    Finihed Web app

---
+++ js/api/constants.js
@@ -0,0 +1,7 @@
+const API_KEY = "d7de5c8e-d2b0-4c12-b12d-a0b53bfad151";
+const API_URL = `https://todo-api.coderslab.pl/api`;
+
+export {
+    API_URL,
+    API_KEY
+};
---
+++ js/app.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
+import NewTask from "./components/NewTask";
+import Task from "./components/Task";
+import {getTasks} from "./api/tasks";
+
+function App() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        getTasks(setTasks);
+    }, []);
+
+
+    const handleAddNewTask = task => {
+        setTasks(prevTasks => {
+            return [
+                task,
+                ...prevTasks
+            ];
+        });
+    };
+
+
+    const handleRemoveTask = id => {
+        setTasks(prevState => prevState.filter(task => task.id !== id));
+    };
+
+    return (
+        <>
+            <NewTask onNewTask={handleAddNewTask}/>
+
+            {tasks.map(task => {
+                return <Task key={task.id} {...task} onRemoveTask={handleRemoveTask}/>;
+            })}
+        </>
+    );
+}
+
+ReactDOM.render(<App/>, document.querySelector("#app"));
